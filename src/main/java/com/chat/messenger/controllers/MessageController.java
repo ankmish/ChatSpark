405597cb1d1c6ce52b0761871f85a14dc4c31cc9
cef1998 <ankit14498@gmail.com>
2023-07-29 10:37:48 +0530

    Basic session management implementation

--- src/main/java/com/chat/messenger/controllers/MessageController.java
+++ src/main/java/com/chat/messenger/controllers/MessageController.java
@@ -7,6 +7,8 @@ import com.chat.messenger.dto.UserDTO;
 import com.chat.messenger.services.MessageService;
 import com.chat.messenger.services.UserService;
 import com.google.gson.Gson;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,11 +19,14 @@ public class MessageController {
 
     private final UserService userService;
     private final MessageService messageService;
+    private final Map<String, Boolean> loggedInUsers; // Store logged-in users
+
 
     @Autowired
     public MessageController(UserService userService, MessageService messageService) {
         this.userService = userService;
         this.messageService = messageService;
+        this.loggedInUsers = new HashMap<>();
     }
 
     @PostMapping("/create/user")
@@ -40,8 +45,10 @@ public class MessageController {
     public String loginUser(@RequestBody UserDTO userDTO) {
         String username = userDTO.getUsername();
         String passcode = userDTO.getPasscode();
+
         User user = userService.getUser(username);
         if (user != null && user.getPasscode().equals(passcode)) {
+            loggedInUsers.put(username, true);
             return "{\"status\":\"success\"}";
         } else {
             return "{\"status\":\"failure\", \"message\":\"Invalid username or passcode\"}";
@@ -50,7 +57,7 @@ public class MessageController {
 
     @GetMapping("/get/users")
     public String getUsers(@RequestParam String username) {
-        if (isUserLoggedIn(username)) {
+        if (isLoggedIn(username)) {
             List<String> usernames = userService.getAllUsernames();
             return "{\"status\":\"success\", \"data\":" + toJson(usernames) + "}";
         } else {
@@ -61,7 +68,7 @@ public class MessageController {
     @GetMapping("/get/unread")
     public String getUnreadMessages(@RequestParam String username) {
         // Check if the user is logged in
-        if (isUserLoggedIn(username)) {
+        if (isLoggedIn(username)) {
             List<Message> unreadMessages = messageService.getUnreadMessages(username);
             return "{\"status\":\"success\", \"message\":\"You have message(s)\", \"data\":" +
                     toJson(unreadMessages) + "}";
@@ -73,16 +80,20 @@ public class MessageController {
     @PostMapping("/send/text/user")
     public String sendMessage(@RequestBody MessageDTO messageDTO) {
         String sender = messageDTO.getFrom();
+        // Check if the sender is logged in
+        if (!isLoggedIn(sender)) {
+            return "{\"status\":\"failure\", \"message\":\"User not logged in\"}";
+        }
+
         String recipient = messageDTO.getTo();
         String text = messageDTO.getText();
         messageService.sendMessage(sender, recipient, text);
         return "{\"status\":\"success\"}";
     }
-
     @GetMapping("/get/history")
     public String getChatHistory(@RequestParam String user, @RequestParam String friend) {
         // Check if the user is logged in
-        if (isUserLoggedIn(user)) {
+        if (isLoggedIn(user)) {
             List<Message> chatHistory = messageService.getChatHistory(user, friend);
             return "{\"status\":\"success\", \"data\":" + toJson(chatHistory) + "}";
         } else {
@@ -90,10 +101,22 @@ public class MessageController {
         }
     }
 
-    private boolean isUserLoggedIn(String username) {
-        //assume that the user is logged in if the username is not null or empty.
-        return username != null && !username.isEmpty();
+    @PostMapping("/logout")
+    public String logoutUser(@RequestBody UserDTO userDTO) {
+        String username = userDTO.getUsername();
+        // Check if the user is logged in before logging out
+        if (isLoggedIn(username)) {
+            loggedInUsers.remove(username);
+            return "{\"status\":\"success\"}";
+        } else {
+            return "{\"status\":\"failure\", \"message\":\"User not logged in\"}";
+        }
     }
+
+    private boolean isLoggedIn(String username) {
+        return loggedInUsers.containsKey(username) && loggedInUsers.get(username);
+    }
+
     private String toJson(Object data) {
         return new Gson().toJson(data);
     }
